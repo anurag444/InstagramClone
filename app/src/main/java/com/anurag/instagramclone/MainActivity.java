25011e0a1220cf8e4d41dda6642c65aab38e695c
Anurag Teckchandani <anuragteckchandani@gmail.com>
2019-01-09 21:33:36 +0530

    layout ready

--- app/src/main/java/com/anurag/instagramclone/MainActivity.java
+++ app/src/main/java/com/anurag/instagramclone/MainActivity.java
@@ -1,7 +1,9 @@
 package com.anurag.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import com.parse.ParseInstallation;
@@ -21,7 +23,25 @@ public class MainActivity extends AppCompatActivity  {
         signup =  findViewById(R.id.signup);
         login = findViewById(R.id.login);
 
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+
+        });
+
     }
+
 
 
     }
